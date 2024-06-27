6a075610931cdc9d604fd1fdf812792875d4b157
kemvl <atdayewkemal@gmail.com>
2024-06-27 15:02:59 +0500

    decoding logic is added

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/fiorix/go-smpp/smpp"
 	"github.com/fiorix/go-smpp/smpp/pdu"
 	"github.com/fiorix/go-smpp/smpp/pdu/pdufield"
+	"github.com/warthog618/sms/encoding/ucs2"
 )
 
 var logInstance *logger.Loggers
@@ -28,6 +29,7 @@ func main() {
 
 	logInstance.InfoLogger.Info("Server is up and running")
 
+	// Initialize RabbitMQ
 	rabbitMQ, err = rabbitmq.NewRabbitMQ(cfg.RabbitMQ)
 	if err != nil {
 		logInstance.ErrorLogger.Error("failed to set up RabbitMQ: %v", err)
@@ -52,10 +54,29 @@ func main() {
 }
 
 func handlerFunc(p pdu.Body) {
+	if p.Header().ID != pdu.DeliverSMID {
+		return
+	}
+
 	f := p.Fields()
 	src := f[pdufield.SourceAddr].String()
 	dst := f[pdufield.DestinationAddr].String()
-	txt := f[pdufield.ShortMessage].String()
+	shortMessage := f[pdufield.ShortMessage].Bytes()
+
+	// Check the PDU header ID to determine the message type
+	pduID := p.Header().ID
+
+	// Decode the short message based on PDU ID and DCS
+	var txt string
+	switch pduID {
+	case pdu.DeliverSMID:
+		// Extract the DCS (Data Coding Scheme)
+		dcs := f[pdufield.DataCoding].Bytes()[0]
+		txt = decodeShortMessage(shortMessage, dcs)
+	default:
+		logInstance.ErrorLogger.Error(fmt.Sprintf("Unsupported PDU ID: %d", pduID))
+		return
+	}
 
 	message := fmt.Sprintf("Received DeliverSM from=%s to=%s: %s", src, dst, txt)
 	logInstance.InfoLogger.Info(message)
@@ -74,4 +95,24 @@ func handlerFunc(p pdu.Body) {
 	if err != nil {
 		logInstance.ErrorLogger.Error(fmt.Sprintf("Failed to publish message to RabbitMQ (%s): %v", queueName, err))
 	}
+}
+
+func decodeShortMessage(shortMessage []byte, dcs byte) string {
+	var txt string
+	switch dcs {
+	case 0x00:
+		txt = string(shortMessage) // Default encoding (7-bit)
+	case 0x08:
+		// UCS2 encoding (16-bit)
+		ucs2Text, err := ucs2.Decode(shortMessage)
+		if err != nil {
+			logInstance.ErrorLogger.Error(fmt.Sprintf("Failed to decode UCS2 message: %v", err))
+			return ""
+		}
+		txt = string(ucs2Text)
+	default:
+		logInstance.ErrorLogger.Error(fmt.Sprintf("Unsupported DCS: %d", dcs))
+		return ""
+	}
+	return txt
 }
